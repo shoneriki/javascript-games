a5ea278f9fe47f3b1cddf25ca823036d92905ecd
shoneriki <ikirenohs@gmail.com>
2020-12-30 22:55:44 +0900

    snake moves

--- snake/app.js
+++ snake/app.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const squares = document.querySelector('.grid div')
+  const squares = document.querySelectorAll('.grid div')
   const scoreDisplay = document.querySelector('span')
-  const startBtn = document.querySelector('start')
+  const startBtn = document.querySelector('.start')
 
   const width = 10
   let currentIndex = 0
-  let appleIindex = 0
-  let currentSnake = [2, 1, 0]
+  let appleIndex = 0
+  let currentSnake = [2,1,0]
   let direction = 1
   let score = 0
   let speed = 0.9
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     direction = 1
     scoreDisplay.innerText = score
     intervalTime = 1000
-    currentSnake = [2, 1, 0]
+    currentSnake = [2,1,0]
     currentIndex = 0
     currentSnake.forEach(index => squares[index].classList.add('snake'))
     interval = setInterval(moveOutcomes, intervalTime)
@@ -47,6 +47,18 @@ document.addEventListener('DOMContentLoaded', () => {
     currentSnake.unshift(currentSnake[0] + direction) //gives direction to head of array
     //deals with snake eating apple
 
+    if(squares[currentSnake[0]].classList.contains('apple')) {
+      squares[currentSnake[0]].classList.remove('apple')
+      squares[tail].classList.add('snake')
+      currentSnake.push(tail)
+      // randomApple()
+      score++
+      scoreDisplay.textContent = score
+      clearInterval(interval)
+      intervalTime = intervalTime * speed
+      interval = setInterval(moveOutcomes, intervalTime)
+    }
+    squares[currentSnake[0]].classList.add('snake')
   }
 
 
@@ -56,15 +68,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if(e.keyCode === 39) {
       direction = 1 // right arrow
-    } else if (e.keycode === 38) {
+    } else if (e.keyCode === 38) {
       direction = -width // up arrow
-    } else if (e.keycode === 37) {
-      direction -1 // left arrow
+    } else if (e.keyCode === 37) {
+      direction = -1 // left arrow
     } else if (e.keyCode === 40) {
       direction = +width //down arrow
     }
   }
 
   document.addEventListener('keyup', control)
+  startBtn.addEventListener('click', startGame)
 
 })
