5eeb2c86211834f1a85534a627ece2c858c47af0
shoneriki <ikirenohs@gmail.com>
2020-12-30 23:04:20 +0900

    game functional

--- snake/app.js
+++ snake/app.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     squares[appleIndex].classList.remove('apple')
     clearInterval(interval)
     score = 0
-    //randomApple()
+    randomApple()
     direction = 1
     scoreDisplay.innerText = score
     intervalTime = 1000
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[currentSnake[0]].classList.remove('apple')
       squares[tail].classList.add('snake')
       currentSnake.push(tail)
-      // randomApple()
+      randomApple()
       score++
       scoreDisplay.textContent = score
       clearInterval(interval)
@@ -59,6 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
       interval = setInterval(moveOutcomes, intervalTime)
     }
     squares[currentSnake[0]].classList.add('snake')
+  }
+
+  //generate apple
+  function randomApple() {
+    do {
+      appleIndex = Math.floor(Math.random() * squares.length)
+    } while(squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
   }
 
 
