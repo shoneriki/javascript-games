85a2388d82258402b8ce93226c8849546ed4e3b0
shoneriki <ikirenohs@gmail.com>
2021-01-01 19:11:22 +0900

    ship moves

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -34,10 +34,12 @@ document.addEventListener("DOMContentLoaded", () => {
     switch(e.keyCode) {
       case 37:
         if(currentShooterIndex % width !== 0) currentShooterIndex -= 1
+        break
       case 39:
         if(currentShooterIndex % width < width -1) currentShooterIndex += 1
         break
-    }
+      }
     squares[currentShooterIndex].classList.add("shooter")
     }
+    document.addEventListener("keydown", moveShooter)
 })
