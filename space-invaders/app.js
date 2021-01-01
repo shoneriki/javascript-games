ce4e81704490c2cdc6ac6dfdf8db91b2b9d4a2b3
shoneriki <ikirenohs@gmail.com>
2021-01-01 15:40:57 +0900

    space-invaders boilerplate

---
+++ space-invaders/app.js
@@ -0,0 +1,5 @@
+// keys for moving player horizontally left and right
+// cannon controlled by player
+// lasers being shot from cannon, moves vertically, bottom to up
+// lasers make enemies off screen when hit
+// enemies move in zig-zag fashion whilst moving downward
