d894718d257044930ab5b69900355b18bc54ca31
shoneriki <ikirenohs@gmail.com>
2021-01-02 17:51:49 +0900

    selectors JS

---
+++ frogger/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div')
+  const timeLeft = document.querySelector('#time-left')
+  const result = document.querySelector('#result')
+  const startBtn = document.querySelector('#button')
+  const width = 9
+  let currentIndex = 76
+
+
+
+})
