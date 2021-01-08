c59a9586a69524df2b322adee9bf38861f1af20c
shoneriki <ikirenohs@gmail.com>
2021-01-08 18:15:27 +0900

    losing function

--- simple-game/script.js
+++ simple-game/script.js
@@ -9,3 +9,16 @@ function jump() {
     character.classList.remove("animate")
   }, 500)
 }
+
+const checkDead = setInterval(function() {
+  var characterTop=
+  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockLeft=
+  parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+    block.style.animation= "none";
+    block.style.display= "none";
+    character.style.display="none";
+    alert("You lost!");
+  }
+},10);
