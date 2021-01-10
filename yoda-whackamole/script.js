a2af1e76c310822fd059506184da4d9691bae749
shoneriki <ikirenohs@gmail.com>
2021-01-10 18:37:43 +0900

    moles popout

--- yoda-whackamole/script.js
+++ yoda-whackamole/script.js
@@ -1,4 +1,4 @@
-const hole = document.querySelectorAll('.hole');
+const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('score')
 const mole = document.querySelectorAll('mole');
 const countdownBoard = document.querySelector('.countdown');
@@ -10,13 +10,23 @@ let timeLimit = 20000;
 let score = 0;
 let countdown;
 
-
 function pickRandomHole(holes) {
   const randomHole = Math.floor(Math.random() * holes.length);
   const hole = holes[randomHole];
   if (hole === lastHole) {
-    return pickRandomHole(hole);
+    return pickRandomHole(holes);
   }
   lastHole = hole;
   return hole;
 }
+function popOut(){
+  const time = Math.random() * 1300 + 400;
+  const hole = pickRandomHole(holes);
+  hole.classList.add('up');
+  setTimeout(function(){
+    hole.classList.remove('up');
+    if(!timeUp) popOut();
+  }, time);
+}
+
+popOut();
