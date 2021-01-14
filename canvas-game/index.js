4a9b0b68cc7ab2b80c7805ad66438768cb4392a0
shoneriki <ikirenohs@gmail.com>
2021-01-14 16:09:28 +0900

    player is blue circle

--- canvas-game/index.js
+++ canvas-game/index.js
@@ -1,6 +1,35 @@
 const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
 
 // create player
+  //color
+  //size
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+const player = new Player(100, 100, 30, 'blue')
+
+player.draw()
+
+console.log(player)
+
+
 // shoot
 // create enemies
 // detect hit/ hit by bullet
