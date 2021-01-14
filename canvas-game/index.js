f2bdc5ddef3fa9ea6e534443bbcd3fb298e5f245
shoneriki <ikirenohs@gmail.com>
2021-01-14 15:52:59 +0900

    pseudocode

---
+++ canvas-game/index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas');
+
+// create player
+// shoot
+// create enemies
+// detect hit/ hit by bullet
+// detect hit on player
+// remove projectiles that are off-screen
+// color game
+// shrink enemies when hit
+// create explosion for enemy when it dies
+// add score
+// add game over ui
+// restart button
+// start game button
