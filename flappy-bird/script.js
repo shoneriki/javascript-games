1e964c7556409d5dcf04e4753f479c6bc598aa1f
shoneriki <ikirenohs@gmail.com>
2021-01-17 18:50:52 +0900

    gravity

--- flappy-bird/script.js
+++ flappy-bird/script.js
@@ -1,17 +1,30 @@
 // select (game?), block, hole, bird
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
-var transparent = document.getElement
+var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
   // hole random heights
-  var random = -((Math.random()*300)+150);
-  hole.style.top = random + "px";
+  var random = Math.random() * 3;
+  var top = (random*100)+150;
+  hole.style.top = -(top) + "px";
 });
+setInterval(function(){
+  // get top of character/bird
+  var characterTop =
+  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  if(jumping==0){
+    // bird falls when nothing is pressed
+    character.style.top = (characterTop +3) + "px";
+  }
+
+}, 10);
+
 
 // bird jumps
 // if bird goes through hole logic
 // timer? score?
-// if bird hits block edge, game stops
-//  game stops
-//  you lose
+// game over/you lose
+  // if bird hits block edge
+  // if bird hits ground
