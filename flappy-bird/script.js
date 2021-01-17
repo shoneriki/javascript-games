725ce2024ea54dce4217fc0961378b6c9ec362ba
shoneriki <ikirenohs@gmail.com>
2021-01-17 19:20:46 +0900

    click working

--- flappy-bird/script.js
+++ flappy-bird/script.js
@@ -1,25 +1,39 @@
-// select (game?), block, hole, bird
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
-  // hole random heights
   var random = Math.random() * 3;
   var top = (random*100)+150;
   hole.style.top = -(top) + "px";
 });
+
 setInterval(function(){
-  // get top of character/bird
   var characterTop =
   parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   if(jumping==0){
-    // bird falls when nothing is pressed
     character.style.top = (characterTop +3) + "px";
   }
+},10);
 
-}, 10);
+  function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+      var characterTop =  parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+      if((characterTop>6)&&(jumpCount<15)){
+        character.style.top = (characterTop - 5) + "px";
+      }
+      if(jumpCount>20) {
+        clearInterval(jumpInterval);
+        jumping=0;
+        jumpCount=0;
+      }
+      jumpCount++
+  },10);
+}
+
 
 
 // bird jumps
