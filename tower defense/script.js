519b1707c01ed3508762688359b517eeb9941a77
shoneriki <ikirenohs@gmail.com>
2021-02-01 14:10:17 +0900

    cells created

--- tower defense/script.js
+++ tower defense/script.js
@@ -12,6 +12,31 @@ const controlsBar = {
   width: canvas.width,
   height: cellSize,
 }
+class Cell{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+  }
+  draw() {
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+  }
+}
+function createGrid(){
+  for( let y = cellSize; y < canvas.height; y += cellSize){
+    for(let x = 0; x < canvas.width; x += cellSize){
+      gameGrid.push(new Cell(x, y));
+    }
+  }
+}
+createGrid();
+function handleGameGrid(){
+  for(let i = 0; i < gameGrid.length; i++){
+    gameGrid[i].draw();
+  }
+}
 // projectiles
 // defenders
 // enemies
@@ -20,6 +45,7 @@ const controlsBar = {
 function animate(){
   ctx.fillStyle= 'blue';
   ctx.fillRect(0,0, controlsBar.width, controlsBar.height);
+  handleGameGrid();
   requestAnimationFrame(animate);
 }
 animate();
