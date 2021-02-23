6e9742c4c528c2cce9454d5b37c88dda8124a478
shoneriki <ikirenohs@gmail.com>
2021-02-23 14:55:37 +0900

    can add X's at the moment, circles still false

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,9 +1,23 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
 
 cellElements.forEach(cell => {
   cell.addEventListener('click', handleClick, { once: true })
 })
 
+// 1
 function handleClick(e) {
-  console.log('clicked');
+  // 1.1 placeMark
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  // 1.2 check for win
+  // 1.3 check for draw
+  // 1.4 switch turns
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
 }
