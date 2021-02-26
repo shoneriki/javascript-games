16784e841b2e1b5fea8bf28ae681c6d4785ad2dc
shoneriki <ikirenohs@gmail.com>
2021-02-26 19:44:57 +0900

    platforms

--- doodle-jump/app.js
+++ doodle-jump/app.js
@@ -2,14 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
-  let  doodlerBottomSpace = 150
+  let doodlerBottomSpace = 150
+  let isGameOver = false
+  let platformCount = 5
 
-  // 1st function
+  // 1.function
   function createDoodler() {
     grid.appendChild(doodler)
     doodler.classList.add('doodler')
     doodler.style.left =  doodlerLeftSpace + 'px'
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
-  createDoodler()
+
+  // 1.constructor
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom
+      this.left = Math.random() * 315
+      this.visual = document.createElement('div')
+
+      const visual = this.visual
+      visual.classList.add('platform')
+      visual.style.left = this.left + 'px'
+      visual.style.bottom = this.bottom + 'px'
+      grid.appendChild(visual)
+    }
+  }
+
+  // 1.1 function
+  function createPlatforms() {
+    for (let i = 0; i < platformCount; i++) {
+      let platGap = 600 / platformCount
+      let newPlatBottom = 100 + i * platGap
+      let newPlatform = new Platform(newPlatBottom)
+    }
+  }
+
+  // 2.function
+  function start() {
+    if(!isGameOver) {
+      createDoodler()
+      createPlatforms()
+    }
+    // put as button?
+  }
+  start()
 })
