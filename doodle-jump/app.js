fe9ed74e087da22ca01ec084a413eafdc2c2a849
shoneriki <ikirenohs@gmail.com>
2021-02-26 19:15:38 +0900

    primitive doodler in grid

---
+++ doodle-jump/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  const doodler = document.createElement('div')
+  let doodlerLeftSpace = 50
+  let  doodlerBottomSpace = 150
+
+  // 1st function
+  function createDoodler() {
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodler.style.left =  doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
+  createDoodler()
+})
